dbd086a3431de3754119c6cb5dde23875115d220
Faisal ahmed <faisal2410@yahoo.com>
2023-07-29 01:24:23 +0600

    updated

---
+++ note.js
@@ -0,0 +1,20 @@
+// npm init vue @latest
+
+// content: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+
+// ],
+
+
+
+// env: {
+//     node: true
+// }
+
+
+// npm install - g json - server
+
+// json - server--watch database/db.json --port=8000
+
+// win+shift+arrow
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
-  <header>
-    <div >    
-     
-<h1 class="text-3xl text-center text-green-700 bg-red-100 font-bold underline">Hello world</h1>
-      <nav class="flex justify-center items-center text-2xl mt-4">
-        <RouterLink class="mr-[30px] hover:text-blue-300 transition-all " to="/">Home</RouterLink>
-        <RouterLink class="hover:text-blue-300 mr-[30px]" to="/about">About</RouterLink>
-        <RouterLink  class="hover:text-blue-300" to ="/movies">Movies</RouterLink>
-        
-      </nav>
-    </div>
-  </header>
+ 
 
   <div class="max-w-7xl mx-auto">
-
+<Navbar></Navbar>
     <RouterView />
   </div>
 </template>
