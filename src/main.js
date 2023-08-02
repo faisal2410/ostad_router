2f88558e412a2d73f8fda2e7cc8b1099c2484a39
Faisal ahmed <faisal2410@yahoo.com>
2023-08-02 18:50:37 +0600

    updated

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp,markRaw } from 'vue'
+import { createPinia } from "pinia";
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const pinia = createPinia();
+pinia.use(({ store }) => {
+    store.router = markRaw(router);
+});
 
+const app = createApp(App)
+app.use(pinia);
 app.use(router)
 
 app.mount('#app')
---
+++ src/stores/movies.js
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia";
+
+export const useMoviesStore = defineStore("movies", {
+    state: () => ({
+        movies: [],
+        isLoading: false,
+        query: "",
+        singleMovie: {},
+        year: 2000,
+    }),
+    getters: {
+        totalMovies() {
+            return this.filterMovies.length;
+        },
+        filterMovies() {
+            return this.movies.filter((movie) => {
+                return movie.year >= this.year;
+            });
+        },
+    },
+    actions: {
+        async getMovies() {
+            this.isLoading = true;
+            const result = await fetch(
+                "http://localhost:8000/movies?q=" + this.query
+            );
+            const response = await result.json();
+            this.movies = response;
+            this.isLoading = false;
+        },
+        async getSingleMovie(id) {
+            this.isLoading = true;
+            const result = await fetch(
+                `http://localhost:8000/movies/${parseInt(id)}`
+            );
+            if (result.status === 404) {
+                this.router.push({ name: "NotFound" });
+            }
+            const response = await result.json();
+            this.singleMovie = response;
+            this.isLoading = false;
+        },
+    },
+});
