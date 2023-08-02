3116a331f229fca833b3b12ba79b4684f11b0a7f
Faisal ahmed <faisal2410@yahoo.com>
2023-08-02 19:11:25 +0600

    updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
     <script setup>
-    import { watchEffect } from "vue";
+    import { watchEffect ,RouterLink} from "vue";
 import { useMoviesStore } from "../stores/movies";
 
 const moviesStore = useMoviesStore();
@@ -11,38 +11,6 @@ watchEffect(() => {
 <template>
 
     
-<!-- <nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center">
-      
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Vue Router</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-
-
-
-    
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <RouterLink to="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/about" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">About</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/movies" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Movies</RouterLink>
-        </li>
-      
-      </ul>
-    </div>
-  </div>
-</nav> -->
 
 
 
@@ -61,7 +29,7 @@ watchEffect(() => {
       class="container flex flex-wrap justify-between items-center mx-auto"
       bis_skin_checked="1"
     >
-      <a href="https://flowbite.com/" class="flex items-center">
+      <RouterLink :to="{ name: 'Home' }" class="flex items-center">
         <span
           class="
             self-center
@@ -70,9 +38,9 @@ watchEffect(() => {
             whitespace-nowrap
             dark:text-white
           "
-          >Pinia</span
+          >Ostad Pinia</span
         >
-      </a>
+      </RouterLink>
       <div class="flex md:order-2" bis_skin_checked="1">
         <button
           type="button"
